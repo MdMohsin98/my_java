a585750523926bc655acbdf700950eb7afaadf55
Mohammad Mohsin <mohammad.mohsin98012@gmail.com>
2023-11-16 15:11:06 +0530

    new file added

---
+++ wrapper_class.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class wrapper_class {
+    public static void main(String[] args) {
+        // Wrapper class in java
+
+        int i = 10;
+        Integer j = new Integer(10); // Wrapper Class
+        Integer k = Integer.valueOf(20); // Wrapper Class
+        System.out.println(j);
+        System.out.println(i);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        String name1 = sc.nextLine();
+        double a = sc.nextDouble();
+        int b = sc.nextInt();
+        System.out.println(name1);
+        System.out.println(a);
+        System.out.println(b);
+        sc.close();
+
+    }
+    
+}
