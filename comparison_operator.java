59dcbaa11ee2300e1a67c6fe2f079bdfb4d3d0bd
Mohammad Mohsin <mohammad.mohsin98012@gmail.com>
2023-11-25 16:01:37 +0530

    Solved some problems and added new files.

--- comparison_operator.java
+++ comparison_operator.java
@@ -1,9 +1,8 @@
-public class comparison_operators {
+public class comparison_operator {
     public static void main(String[] args) {
         // Comparison operators
         //Comparison operators returns either true or false
         //        ==        >=      <=      <       >       !=
-
         System.out.println(5==9); // false
         System.out.println(3==3); // true
         System.out.println(3>=5); // false
---
+++ time_and_space_complexity.java
@@ -0,0 +1,18 @@
+public class time_and_space_complexity {
+    public static void main(String[] args) {
+        // Time and space complexity
+
+        // int[] a = {1,2,3,4,5};
+        // for (int i = 0; i<= a.length-1; i++){
+        //     if (a[i]<5) {
+        //         System.out.println(a[i]);                
+        //     }
+        // }
+        // for (int i = 1;i<=5;i++){
+        //     for(int j = 1; j<= 5;j++){
+        //         System.out.print(1);
+        //     }
+        // }
+    }
+    
+}
